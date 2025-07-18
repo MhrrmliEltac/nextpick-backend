87fae26000c1a8ce2d0db0a22c9e65f9d3765452
MhrrmliEltac <meherremlieltac14@gmail.com>
2025-07-18 12:22:37 +0400

    cors error solved

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://next-pick-commerce.vercel.app/"],
+    origin: ["http://localhost:5173", "https://next-pick-commerce.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
