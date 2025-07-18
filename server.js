511d913af245f6e662a1a419f3f1350c214e850d
MhrrmliEltac <meherremlieltac14@gmail.com>
2025-07-18 12:20:30 +0400

    added cors

--- server.js
+++ server.js
@@ -17,7 +17,12 @@ connectDb();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://next-pick-commerce.vercel.app/"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
