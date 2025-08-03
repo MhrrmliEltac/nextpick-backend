fbc563c7ac8fbe8296bb35030ef04415c1962709
MhrrmliEltac <meherremlieltac14@gmail.com>
2025-08-04 02:22:02 +0400

    cors error solved

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ import blogRoutes from "./routes/blog.routes.js";
 import infoRoutes from "./routes/info.routes.js";
 import subCategoryRoutes from "./routes/subcategory.routes.js";
 import apiKeyRoutes from "./routes/apikey.routes.js";
-import {apiKeyMiddleware} from "./middleware/apiKeyMiddleware.js";
+import { apiKeyMiddleware } from "./middleware/apiKeyMiddleware.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -24,9 +24,9 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-      origin: ["http://localhost:5173", "http://localhost:5174", "https://next-pick-commerce.vercel.app"],
+    origin: ["http://localhost:5173", "https://next-pick-commerce.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
-      credentials: true,
+    credentials: true,
   })
 );
 app.use(express.json({ limit: "10mb" }));
